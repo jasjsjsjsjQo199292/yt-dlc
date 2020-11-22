adf2226b67772bb902e4104776472d6c68fb525d
jasjsjsjsjQo199292 <74087082+jasjsjsjsjQo199292@users.noreply.github.com>
2020-11-22 14:47:26 -0800

    Update furaffinity.py

--- youtube_dlc/extractor/furaffinity.py
+++ youtube_dlc/extractor/furaffinity.py
@@ -30,30 +30,28 @@ class FuraffinityIE(InfoExtractor):
           'id': '32332269',
           'ext': 'png',
           'title': 'Make a Wish',
-          'description': 'Feral fullbody for:MrSilverFox 
-I am having sooooo much fun with these. Does it show? Because they seriously just
-fill me with happiness every single time! I
-definitely plan to open for more in the future!',
-          'upload_date': 20190719
-          'uploader': PaceVanRign
+          'description': '',
+   !',
+          'upload_date': '20190719'
+          'uploader': 'PaceVanRign'
           'age_limit': '0'
-          'view_count': int,
+          'view_count': 'int',
         },
         'skip': 'video gone',
          }, {
  # age-restricted
-   'url': https://www.furaffinity.net/view/39326918/
+   'url': 'https://www.furaffinity.net/view/39326918/'
    'info_dict': {
             'id': '39326918',
             'ext': 'jpg',
             'title': 'Ych more reminder',
             'uploader': 'Puffic',
-            'age_limit': 18,
+            'age_limit': '18',
         },
         'skip': 'video gone',
         }, {
         'url': 'https://www.furaffinity.net/view/19883645/',
-        'only_matching': True,
+        'only_matching': 'True',
         }
         
 
